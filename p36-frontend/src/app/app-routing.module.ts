c5421d82c06b35e767a0922ca5697ea4d70bd48a
Andras Kelemen <kelemenandras11@gmail.com>
2019-03-16 14:31:35 +0100

    login, registration, startpage components

--- p36-frontend/src/app/app-routing.module.ts
+++ p36-frontend/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { StartPageComponent } from './startpage/startpage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
 
+    { path: '', component: StartPageComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'registration', component: RegistrationComponent },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
