c6d370f920857b881eef802e856eb9154fb9c88f
Andras Kelemen <kelemenandras11@gmail.com>
2019-03-16 18:48:26 +0100

    registration-forms

--- p36-frontend/src/app/app.module.ts
+++ p36-frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { StartPageComponent } from './startpage/startpage.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { StartPageComponent } from './startpage/startpage.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- p36-frontend/src/app/registration/registration.component.ts
+++ p36-frontend/src/app/registration/registration.component.ts
@@ -1,15 +1,92 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnChanges, SimpleChanges, ViewChild} from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.css']
+    selector: 'app-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+    regForm = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        emailAddress: ['', Validators.required/*, Validators.email*/],
+        address: this.fb.group({
+            country: ['', Validators.required],
+            state: [''],
+            zip: ['', Validators.required],
+            city: ['', Validators.required],
+            street: ['', Validators.required]
+        }),
+    });
+    //@ViewChild('regForm') regForm;
 
-  constructor() { }
 
-  ngOnInit() {
-  }
 
+    // regForm = new FormGroup({
+    //     firstName: new FormControl(''),
+    //     lastName: new FormControl(''),
+    //     emailAddress: new FormControl(''),
+    //     country: new FormControl(''),
+    //     state: new FormControl(''),
+    //     zip: new FormControl(''),
+    //     city: new FormControl(''),
+    //     street: new FormControl('')
+    // });
+    //firstName = new FormControl('');
+    // lastName = new FormControl('');
+
+    personalForm: boolean;
+    addressForm: boolean;
+    lastNameField: any;
+    emailField: any;
+    firstNameField: any;
+    private personalValid: boolean;
+
+    constructor(private fb: FormBuilder) { }
+
+    ngOnInit() {
+        this.personalForm = true;
+        this.addressForm = false;
+        this.lastNameField = this.regForm.get('lastName');
+        this.firstNameField = this.regForm.get('firstName');
+        this.emailField = this.regForm.get('emailAddress');
+        this.onChanges();
+        this.personalValid = false;
+    }
+
+
+    goToNextStep(): void {
+        // (this.lastNameField.invalid || this.firstNameField.invalid || this.emailField.invalid)
+        this.personalForm = false;
+        this.addressForm = true;
+    }
+
+
+    submitRegData() {
+
+    }
+
+    onSubmit() {
+        console.warn(this.regForm.value);
+        console.warn(this.regForm.status);
+        console.warn(this.regForm.get('lastName').invalid);
+    }
+
+    onChanges(): void {
+            this.regForm.valueChanges.subscribe(val => {
+            this.isPersonalValid()
+        });
+    }
+
+    private isPersonalValid(): void {
+        if(this.lastNameField.valid && this.firstNameField.valid && this.emailField.valid){
+          this.personalValid = true;
+        }else{
+            this.personalValid = false;
+        }
+    }
 }
+
