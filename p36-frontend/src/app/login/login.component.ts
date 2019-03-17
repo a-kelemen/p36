49d9c9a89ca48e2fb5cc944de3a35a026b1677ad
Andras Kelemen <kelemenandras11@gmail.com>
2019-03-17 10:30:31 +0100

    login fields

--- p36-frontend/src/app/login/login.component.ts
+++ p36-frontend/src/app/login/login.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
+import {Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {ifTrue} from "codelyzer/util/function";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginFormIsValid: boolean;
+    loginForm = this.fb.group({
+        emailAddress: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required],
 
-  ngOnInit() {
-  }
+    });
 
+    constructor(private fb: FormBuilder, private router: Router) {
+    }
+
+    ngOnInit() {
+        this.loginFormIsValid = false;
+        this.onChanges();
+    }
+
+    onChanges(): void {
+        this.loginForm.valueChanges.subscribe(val => {
+            if (this.loginForm.get("password").valid && this.loginForm.get("emailAddress").valid) {
+                this.loginFormIsValid = true;
+            } else {
+                this.loginFormIsValid = false;
+            }
+        });
+    }
+
+    isEmailInvalid(): boolean {
+        return this.loginForm.get("emailAddress").invalid && this.loginForm.get("emailAddress").dirty;
+    }
+
+    isPasswordInvalid(): boolean {
+        return this.loginForm.get("password").invalid && this.loginForm.get("password").dirty;
+    }
+
+    login(): void{
+        console.warn(this.loginForm.value);
+        this.router.navigate(["/dashboard"]);
+    }
 }
