aa2723fb8a18be65e2af099483596dc0e169b4ee
Dávid Bertalan <oberheitzer@David-MacBook-Pro.local>
2019-04-07 01:48:15 +0200

    user model, controller, service modified for authentication

--- p36-backend/src/main/java/hu/neo/bank/p36backend/controller/UserController.java
+++ p36-backend/src/main/java/hu/neo/bank/p36backend/controller/UserController.java
@@ -4,10 +4,7 @@ import hu.neo.bank.p36backend.model.User;
 import hu.neo.bank.p36backend.service.UserServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -18,18 +15,25 @@ public class UserController {
     @Autowired
     private UserServiceInterface userService;
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+    @CrossOrigin
     @PostMapping("/registration")
     public ResponseEntity<User> createUser(@RequestBody User user) {
-        Optional<User> optionalUser = userService.findByUsername(user.getUsername()); //must check
+        Optional<User> u = userService.findByUsername(user.getUsername()); //must check
 
         // CHECK IF USER EXISTS
-        if(optionalUser.isPresent()) {
+        //if(optionalUser.isPresent()) {
 
-        }
+        //}
 
         return ResponseEntity.ok(userService.createUser(user));
     }
 
+    @CrossOrigin
     @PostMapping("/login")
     public ResponseEntity<User> login(@RequestBody User user) {
         User u = userService.findByUsername(user.getUsername()).get(); //must check
--- p36-backend/src/main/java/hu/neo/bank/p36backend/repository/UserRepositoryInterface.java
+++ p36-backend/src/main/java/hu/neo/bank/p36backend/repository/UserRepositoryInterface.java
@@ -2,15 +2,16 @@ package hu.neo.bank.p36backend.repository;
 
 import hu.neo.bank.p36backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-
+@Repository
 public interface UserRepositoryInterface extends JpaRepository<User, Integer> {
 
     Optional<User> findById(Integer id);
 
-    User findByUsername(String name);
+    Optional<User> findByUsername(String name);
 
     User save(User user);
 }
